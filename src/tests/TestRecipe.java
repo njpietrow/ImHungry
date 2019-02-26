3dd967bdd018277b47ee23bfebd3243a7e74b249
Nick Pietrow <pietrow@usc.edu>
2019-02-26 12:01:20 -0800

    added test

---
+++ src/tests/TestRecipe.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestRecipe {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
