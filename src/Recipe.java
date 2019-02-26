95a088768686da70cc0033ecb929752ad0379fe9
Kevin Calaway <calaway@usc.edu>
2019-02-26 11:30:36 -0800

    First commit.

---
+++ src/Recipe.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+
+public class Recipe extends Result {
+
+	private int prepTime;
+	private int cookTime;
+	private String imgURL;
+	private ArrayList<String> ingredients;
+	private ArrayList<String> instructions;
+	
+	public Recipe(String name, int prepTime, int cookTime, String imgURL, ArrayList<String> ingredients, ArrayList<String> instructions) {
+		super.setName(name);
+		this.prepTime = prepTime;
+		this.cookTime = cookTime;
+		this.imgURL = imgURL;
+		this.ingredients = ingredients;
+		this.instructions = instructions;
+	}
+	
+	public int getPrepTime() {
+		return prepTime;
+	}
+
+	public void setPrepTime(int prepTime) {
+		this.prepTime = prepTime;
+	}
+
+	public int getCookTime() {
+		return cookTime;
+	}
+
+	public void setCookTime(int cookTime) {
+		this.cookTime = cookTime;
+	}
+
+	public String getImgURL() {
+		return imgURL;
+	}
+
+	public void setImgURL(String imgURL) {
+		this.imgURL = imgURL;
+	}
+
+	public ArrayList<String> getIngredients() {
+		return ingredients;
+	}
+
+	public void setIngredients(ArrayList<String> ingredients) {
+		this.ingredients = ingredients;
+	}
+
+	public ArrayList<String> getInstructions() {
+		return instructions;
+	}
+
+	public void setInstructions(ArrayList<String> instructions) {
+		this.instructions = instructions;
+	}
+}
---
+++ src/Restaurant.java
@@ -0,0 +1,70 @@
+
+public class Restaurant extends Result {
+
+	private int driveTime;
+	private double distance;
+	private String websiteURL;
+	private String address;
+	private String phoneNum;
+	private int priceRange;
+	
+	public Restaurant(String name, int driveTime, double distance, String websiteURL, String address, String phoneNum, int priceRange) {
+		super.setName(name);
+		this.driveTime = driveTime;
+		this.distance = distance;
+		this.websiteURL = websiteURL;
+		this.address = address;
+		this.phoneNum = phoneNum;
+		this.priceRange = priceRange;
+	}
+
+	public int getDriveTime() {
+		return driveTime;
+	}
+
+	public void setDriveTime(int driveTime) {
+		this.driveTime = driveTime;
+	}
+
+	public double getDistance() {
+		return distance;
+	}
+
+	public void setDistance(double distance) {
+		this.distance = distance;
+	}
+
+	public String getWebsiteURL() {
+		return websiteURL;
+	}
+
+	public void setWebsiteURL(String websiteURL) {
+		this.websiteURL = websiteURL;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+
+	public int getPriceRange() {
+		return priceRange;
+	}
+
+	public void setPriceRange(int priceRange) {
+		this.priceRange = priceRange;
+	}
+	
+	
+}
