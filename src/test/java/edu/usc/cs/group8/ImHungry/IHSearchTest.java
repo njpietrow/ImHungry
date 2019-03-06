7a099cf60258341f71b189c51500d0ffa57a2aeb
Nick Pietrow <pietrow@usc.edu>
2019-03-06 11:09:17 -0800

    more tests

--- src/test/java/edu/usc/cs/group8/ImHungry/IHSearchTest.java
+++ src/test/java/edu/usc/cs/group8/ImHungry/IHSearchTest.java
@@ -15,85 +15,51 @@ import static org.mockito.Mockito.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class IHSearchTest {
 	
 	@Test
-    public void testServletNormal() throws Exception {
-        HttpServletRequest request = mock(HttpServletRequest.class);       
-        HttpServletResponse response = mock(HttpServletResponse.class);
-        HttpSession session = mock(HttpSession.class);
-        RequestDispatcher RD = mock(RequestDispatcher.class);
-
-
-        when(request.getParameter("search_query")).thenReturn("spaghetti");
-        when(request.getParameter("num_results")).thenReturn("10");
-        when(request.getSession()).thenReturn(session);
-        when(request.getRequestDispatcher("results_page.jsp")).thenReturn(RD);
-        
-        
-        ArrayList<String> images = null;
-		ArrayList<Recipe> recipes = null;
-		ArrayList<Restaurant> restaurants = null;
-        
-
-		StringWriter stringWriter = new StringWriter();
-        PrintWriter writer = new PrintWriter(stringWriter);
-        when(response.getWriter()).thenReturn(writer);
-		
-        new IHSearch().doGet(request, response);      
+    public void testRestaurantSearch() throws Exception {
+       IHSearch IHS = new IHSearch();
+       ArrayList<Restaurant> restaurants = IHS.doRestaurantSearch("spaghetti", "10");
+       assertEquals(restaurants.get(0).getName(),"Lemonade - USC");
+       assertEquals(restaurants.get(1).getName(),"Viztango Cafe");
     }
 	
 	@Test
-    public void testServletNullJsonString() throws Exception {
-        HttpServletRequest request = mock(HttpServletRequest.class);       
-        HttpServletResponse response = mock(HttpServletResponse.class);
-        HttpSession session = mock(HttpSession.class);
-        RequestDispatcher RD = mock(RequestDispatcher.class);
-
-
-        when(request.getParameter("search_query")).thenReturn("spaghetti");
-        when(request.getParameter("num_results")).thenReturn("10");
-        when(request.getSession()).thenReturn(session);
-        when(request.getRequestDispatcher("results_page.jsp")).thenReturn(RD);
-        
-        IHSearch search = mock(IHSearch.class);
-        String keyword = "spaghetti";
-		String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
-				+ "location=34.020593,-118.285447"
-//				+ "&radius=2500"
-				+ "&type=restaurant"
-				+ "&rankby=distance"
-				+ "&keyword=" + keyword
-				+ "&key=AIzaSyCe6MRPk3bmzAC476OWtgbH91rJ8hWwRyA\n";
-        when(search.readWebsite(url)).thenReturn(null);
-        
-        assertTrue(search.doRestaurantSearch(keyword, "10") == null);      
+    public void testRecipeSearch() throws Exception {
+       IHSearch IHS = new IHSearch();
+       ArrayList<Recipe> recipes = IHS.doRecipeSearch("spaghetti", "10");
+       assertEquals(recipes.get(0).getName(),"Easy Meaty Spaghetti");
+       assertEquals(recipes.get(1).getName(),"Mozzarella Baked Spaghetti");
     }
-	@Test
-    public void testServletNullLists() throws Exception {
-        HttpServletRequest request = mock(HttpServletRequest.class);       
-        HttpServletResponse response = mock(HttpServletResponse.class);
-        HttpSession session = mock(HttpSession.class);
-        RequestDispatcher RD = mock(RequestDispatcher.class);
-
-
-        when(request.getParameter("search_query")).thenReturn("spaghetti");
-        when(request.getParameter("num_results")).thenReturn("10");
-        when(request.getSession()).thenReturn(session);
-        when(request.getRequestDispatcher("results_page.jsp")).thenReturn(RD);
-        
-        IHSearch search = mock(IHSearch.class);
-        ArrayList<String> images = null;
-		ArrayList<Recipe> recipes = null;
-		ArrayList<Restaurant> restaurants = null;
-        
-        when(search.doImageSearch("spaghetti")).thenReturn(images);
-        when(search.doRecipeSearch("spaghetti", "10")).thenReturn(recipes);
-        when(search.doRestaurantSearch("spaghetti", "10")).thenReturn(restaurants);
-        
-        search.doGet(request, response);      
-    }
+	
+	
+//	@Test
+//    public void testServletNullLists() throws Exception {
+//        HttpServletRequest request = mock(HttpServletRequest.class);       
+//        HttpServletResponse response = mock(HttpServletResponse.class);
+//        HttpSession session = mock(HttpSession.class);
+//        RequestDispatcher RD = mock(RequestDispatcher.class);
+//
+//
+//        when(request.getParameter("search_query")).thenReturn("spaghetti");
+//        when(request.getParameter("num_results")).thenReturn("10");
+//        when(request.getSession()).thenReturn(session);
+//        when(request.getRequestDispatcher("results_page.jsp")).thenReturn(RD);
+//        
+//        IHSearch search = mock(IHSearch.class);
+//        ArrayList<String> images = null;
+//		ArrayList<Recipe> recipes = null;
+//		ArrayList<Restaurant> restaurants = null;
+//        
+//        when(search.doImageSearch("spaghetti")).thenReturn(images);
+//        when(search.doRecipeSearch("spaghetti", "10")).thenReturn(recipes);
+//        when(search.doRestaurantSearch("spaghetti", "10")).thenReturn(restaurants);
+//        
+//        search.doGet(request, response);      
+//    }
 	
 
 
