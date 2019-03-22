c5c470d79b1d7bc76008562f9674c31c27e471ba
Kevin Calaway <calaway@usc.edu>
2019-03-22 14:51:29 -0700

    Added LoginHelper

---
+++ src/main/java/edu/usc/cs/group8/ImHungry/LoginHelper.java
@@ -0,0 +1,32 @@
+package edu.usc.cs.group8.ImHungry;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mysql.cj.xdevapi.Statement;
+
+public class LoginHelper {
+    private Connection conn = null;
+    private Statement statement = null;
+    private PreparedStatement preparedStatement = null;
+    private ResultSet resultSet = null;
+
+    try {
+        conn =
+           DriverManager.getConnection("jdbc:mysql://localhost/test?" +
+                                       "user=minty&password=greatsqldb");
+
+        // Do something with the Connection
+
+    } catch (SQLException ex) {
+        // handle any errors
+        System.out.println("SQLException: " + ex.getMessage());
+        System.out.println("SQLState: " + ex.getSQLState());
+        System.out.println("VendorError: " + ex.getErrorCode());
+    }
+    
+
+  }
