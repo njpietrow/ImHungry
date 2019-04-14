6ad26e2a6643b253553be98f05506db21c4da761
Tong Wu <twu781@usc.edu>
2019-04-14 11:32:14 -0700

    fixed search result page buttons

---
+++ cucumbertests/features/features/support/env.rb
@@ -0,0 +1,10 @@
+
+require 'capybara'
+require 'capybara/cucumber'
+require 'rspec'
+
+Capybara.register_driver :chrome do |app|
+    Capybara::Selenium::Driver.new(app, :browser => :chrome,
+                                   :driver_path=>"/Users/tongwu/Downloads/chromedriver")
+end
+Capybara.default_driver = :chrome
