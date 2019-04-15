24bb41422056642be4df5d474b40f108c320cd7a
Chris Hailey <chris.hailey8@gmail.com>
2019-04-14 18:25:56 -0700

    Added Pictures I was supposed to a while back.

--- cucumber_tests/features/support/cuc.rb
+++ cucumber_tests/features/support/cuc.rb
@@ -3,7 +3,7 @@ require 'capybara/cucumber'
 require 'rspec'
 Capybara.register_driver :chrome do |app|
 	Capybara::Selenium::Driver.new(app, :browser => :chrome,
-:driver_path=>"/Users/AahadPatel 1/Downloads/chromedriver")
+:driver_path=>"C:/Desktop/chromedriver.exe")
 end
 
 Capybara.default_driver = :chrome
