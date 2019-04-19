01154ad1af9b0d34f70b6ab2b8441751a2478292
Chris Hailey <chris.hailey8@gmail.com>
2019-04-19 12:45:09 -0700

    Working on Grocery tests right now... still a work in progress. Also added the pair programming selfie

--- src/test/java/edu/usc/cs/group8/ImHungry/GroceryListTest.java
+++ src/test/java/edu/usc/cs/group8/ImHungry/GroceryListTest.java
@@ -19,7 +19,7 @@ public class GroceryListTest {
 		ingredients.add("A can of chicken");
 		ingredients.add("An entire bottle of Sriracha-brand sriracha");
 		Recipe gross = user.get("http://localhost:8080/ImHungry/testrecipe.html");
-		user.getLists().addToGroceryList(gross);
+		user.addToGroceryList(gross);
 		assertTrue(user.getLists().groceryListContains("Like 48 red hots"));
 		assertFalse(user.getLists().groceryListContains("Green beans"));
 		ArrayList<String> moreIngredients = new ArrayList<String>();
@@ -97,7 +97,7 @@ public class GroceryListTest {
 		ingredients.add("A dollop of Daisy");
 		ingredients.add("A can of chicken");
 		Recipe gross = user.get("http://localhost:8080/ImHungry/testrecipe.html");
-		user.getLists().addToGroceryList(gross);
+		user.addToGroceryList(gross);
 		ArrayList<String> moreIngredients = new ArrayList<String>();
 		moreIngredients.add("Green beans");
 		moreIngredients.add("Like 48 red hots");
